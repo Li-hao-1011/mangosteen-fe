2d3cff10662b0d996f6c61e13c3484a25d9d8225
lihao <lihao.coder@foxmail.com>
2022-10-11 23:48:23 +0800

    创建欢迎界面

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,9 +1,23 @@
+import { RouteRecordRaw } from "vue-router";
 import { Bar } from "../views/Bar";
 import { Foo } from "../views/Foo";
+import { Welcome } from "../views/Welcome";
+import { First } from "../components/welcome/First";
+import { Second } from "../components/welcome/Second";
+import { Third } from "../components/welcome/Third";
+import { Forth } from "../components/welcome/Forth";
 
-export const routes = [
+export const routes: RouteRecordRaw[] = [
   { path: "/", component: Foo },
   { path: "/about", component: Bar },
+  {
+    path: "/welcome",
+    component: Welcome,
+    children: [
+      { path: "first", component: First },
+      { path: "second", component: Second },
+      { path: "third", component: Third },
+      { path: "forth", component: Forth },
+    ],
+  },
 ];
-
-

8e5e3dd3e54003abf1899e49026fb493ece77d35
lihao <lihao.coder@foxmail.com>
2022-10-11 22:25:53 +0800

    reset base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/mangosteen-fn/dist/',
+  // base:'/mangosteen-fn/dist/',
   plugins: [
     vue(),
     vueJsx({

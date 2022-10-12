3db2c6d8618ddb726eb682711630b3070aa86949
lihao <lihao.coder@foxmail.com>
2022-10-12 23:52:22 +0800

    修改默认路由

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,6 +1,4 @@
 import { RouteRecordRaw } from "vue-router";
-import { Bar } from "../views/Bar";
-import { Foo } from "../views/Foo";
 import { Welcome } from "../views/Welcome";
 import { First } from "../components/welcome/First";
 import { Second } from "../components/welcome/Second";
@@ -8,13 +6,12 @@ import { Third } from "../components/welcome/Third";
 import { Forth } from "../components/welcome/Forth";
 
 export const routes: RouteRecordRaw[] = [
-  { path: "/", component: Welcome },
-  // { path: "/about", component: Bar },
-
+  { path: "/", redirect: "/welcome" },
   {
     path: "/welcome",
     component: Welcome,
     children: [
+      { path: "", redirect: "/welcome/1" },
       { path: "1", component: First },
       { path: "2", component: Second },
       { path: "3", component: Third },

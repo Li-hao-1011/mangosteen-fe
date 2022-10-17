d303a9e2ddc8bd1e2ee6757228174a48ff53c219
lihao <lihao.coder@foxmail.com>
2022-10-17 17:48:08 +0800

    制作 Center 组件

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # 我的 Vue 3 + TSX 项目
 
+## 编码规范
+
+### ref 默认值
+
+推荐使用
+
+```tsx
+const div = ref<HTMLDivElement>();
+```
+
+不推荐使用
+
+```tsx
+const div = ref<HTMLDivElement | null>(null);
+```
+
+### 坑
+
+1. `Center`组件的 extraClass 不起作用
+
 ## 如何开发
 
-
 ## 如何打包
-
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,6 +1,8 @@
 import { defineComponent } from "vue";
 import { Button } from "../shared/Button";
+import { Center } from "../shared/Center";
 import { FloatButton } from "../shared/FloatButton";
+import { Icon } from "../shared/Icon";
 import s from "./StartPage.module.scss";
 export const StartPage = defineComponent({
   setup: (props, context) => {
@@ -9,6 +11,12 @@ export const StartPage = defineComponent({
     };
     return () => (
       <div>
+        <nav>12312312</nav>
+        <main>
+          <Center class={s.wrapper}>
+            <Icon name="pig" class={s.center_icon} />
+          </Center>
+        </main>
         <div class={s.button_wrapper}>
           <Button class={s.button} onClick={onClick}>
             测试

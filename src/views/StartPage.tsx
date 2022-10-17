969f9355e180919a3a8ccf2c2315a1474d2fdb7f
lihao <lihao.coder@foxmail.com>
2022-10-17 23:38:06 +0800

    添加 Navbar

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -3,6 +3,7 @@ import { Button } from "../shared/Button";
 import { Center } from "../shared/Center";
 import { FloatButton } from "../shared/FloatButton";
 import { Icon } from "../shared/Icon";
+import { Navbar } from "../shared/Navbar";
 import s from "./StartPage.module.scss";
 export const StartPage = defineComponent({
   setup: (props, context) => {
@@ -11,15 +12,20 @@ export const StartPage = defineComponent({
     };
     return () => (
       <div>
-        <nav>12312312</nav>
-        <main>
-          <Center class={s.wrapper}>
-            <Icon name="pig" class={s.center_icon} />
-          </Center>
-        </main>
+        <nav>
+          <Navbar>
+            {{
+              default: () => "山竹记账",
+              icon: () => <Icon name="menu" class={s.nav_icon} />,
+            }}
+          </Navbar>
+        </nav>
+        <Center class={s.wrapper}>
+          <Icon name="pig" class={s.center_icon} />
+        </Center>
         <div class={s.button_wrapper}>
           <Button class={s.button} onClick={onClick}>
-            测试
+            开始记账
           </Button>
         </div>
 

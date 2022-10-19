f90199b579cba27115d85109def8741944d2592f
lihao <lihao.coder@foxmail.com>
2022-10-20 00:28:20 +0800

    准备改变数字键盘的布局

--- src/components/Item/InputPad.tsx
+++ src/components/Item/InputPad.tsx
@@ -26,24 +26,63 @@ export const InputPad = defineComponent({
       refDate.value = date;
       hideDatePicker();
     };
+    const refAmount = ref("");
     const btnMap = [
-      { text: "1", onClick: () => {} },
-      { text: "2", onClick: () => {} },
-      { text: "3", onClick: () => {} },
-      { text: "清空", onClick: () => {} },
-      { text: "4", onClick: () => {} },
-      { text: "5", onClick: () => {} },
-      { text: "6", onClick: () => {} },
-      { text: "+", onClick: () => {} },
-      { text: "7", onClick: () => {} },
-      { text: "8", onClick: () => {} },
-      { text: "9", onClick: () => {} },
-      { text: "-", onClick: () => {} },
-      { text: ".", onClick: () => {} },
-      { text: "0", onClick: () => {} },
-      { text: "删除", onClick: () => {} },
+      {
+        text: "1",
+        onClick: () => appendText(1),
+      },
+      {
+        text: "2",
+        onClick: () => appendText(2),
+      },
+      {
+        text: "3",
+        onClick: () => appendText(3),
+      },
+      {
+        text: "4",
+        onClick: () => appendText(4),
+      },
+      {
+        text: "5",
+        onClick: () => appendText(5),
+      },
+      {
+        text: "6",
+        onClick: () => appendText(6),
+      },
+      {
+        text: "7",
+        onClick: () => appendText(7),
+      },
+      {
+        text: "8",
+        onClick: () => appendText(8),
+      },
+      {
+        text: "9",
+        onClick: () => appendText(9),
+      },
+      {
+        text: ".",
+        onClick: () => appendText("."),
+      },
+      {
+        text: "0",
+        onClick: () => appendText(0),
+      },
+      {
+        text: "清空",
+        onClick: () => {
+          refAmount.value = "0";
+        },
+      },
       { text: "提交", onClick: () => {} },
     ];
+    const appendText = (n: number | string) => {
+      refAmount.value += n.toString();
+    };
     return () => (
       <>
         <div class={s.date_and_amount}>
@@ -67,7 +106,7 @@ export const InputPad = defineComponent({
               </Popup>
             </span>
           </span>
-          <span class={s.amount}>1999.99</span>
+          <span class={s.amount}>{refAmount.value}</span>
         </div>
         <div class={s.buttons}>
           {btnMap.map((button) => (

6667abdd89f869baa4b209b52866ba39746aac62
lihao <lihao.coder@foxmail.com>
2022-10-20 00:08:22 +0800

    安装 vant 病逝用日期选择器

--- src/components/Item/InputPad.tsx
+++ src/components/Item/InputPad.tsx
@@ -2,6 +2,8 @@ import { defineComponent, PropType, ref } from "vue";
 import { Icon } from "../../shared/Icon";
 import { time } from "../../shared/time";
 import s from "./InputPad.module.scss";
+import { DatetimePicker, Popup } from "vant";
+
 export const InputPad = defineComponent({
   props: {
     name: {
@@ -9,27 +11,38 @@ export const InputPad = defineComponent({
     },
   },
   setup: (props, context) => {
-    const refDate = ref<Date>();
     const now = new Date();
+    const refDate = ref<Date>(now);
+    const refTempDate = ref<Date>(now);
+    const refDatePickerVisible = ref(false);
+    const showDatePicker = () => {
+      refTempDate.value = refDate.value;
+      refDatePickerVisible.value = true;
+    };
+    const hideDatePicker = () => {
+      refDatePickerVisible.value = false;
+    };
+    const setDate = (date: Date) => {
+      refDate.value = date;
+      hideDatePicker();
+    };
     const btnMap = [
       { text: "1", onClick: () => {} },
-      { text: "1", onClick: () => {} },
-      { text: "1", onClick: () => {} },
-      { text: "1", onClick: () => {} },
-      { text: "1", onClick: () => {} },
-      { text: "1", onClick: () => {} },
-      { text: "1", onClick: () => {} },
-      { text: "1", onClick: () => {} },
-      { text: "1", onClick: () => {} },
-      { text: "1", onClick: () => {} },
-      { text: "1", onClick: () => {} },
-      { text: "1", onClick: () => {} },
-      { text: "1", onClick: () => {} },
-      { text: "1", onClick: () => {} },
-      { text: "1", onClick: () => {} },
-      { text: "1", onClick: () => {} },
-      { text: "1", onClick: () => {} },
-      { text: "1", onClick: () => {} },
+      { text: "2", onClick: () => {} },
+      { text: "3", onClick: () => {} },
+      { text: "清空", onClick: () => {} },
+      { text: "4", onClick: () => {} },
+      { text: "5", onClick: () => {} },
+      { text: "6", onClick: () => {} },
+      { text: "+", onClick: () => {} },
+      { text: "7", onClick: () => {} },
+      { text: "8", onClick: () => {} },
+      { text: "9", onClick: () => {} },
+      { text: "-", onClick: () => {} },
+      { text: ".", onClick: () => {} },
+      { text: "0", onClick: () => {} },
+      { text: "删除", onClick: () => {} },
+      { text: "提交", onClick: () => {} },
     ];
     return () => (
       <>
@@ -37,12 +50,26 @@ export const InputPad = defineComponent({
           <span class={s.date}>
             <Icon name="date" class={s.icon} />
             <span>
-              <input type="text" value={time(now).format()} />
+              <span onClick={() => showDatePicker()}>
+                {time(refDate.value).format()}
+              </span>
+              <Popup
+                position="bottom"
+                v-model:show={refDatePickerVisible.value}
+              >
+                <DatetimePicker
+                  v-model={refTempDate.value}
+                  type="date"
+                  title="选择年月日"
+                  onConfirm={setDate}
+                  onCancel={hideDatePicker}
+                />
+              </Popup>
             </span>
           </span>
           <span class={s.amount}>1999.99</span>
         </div>
-        <div></div>
+        <div class={s.buttons}>1243123</div>
       </>
     );
   },
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import { svgstore } from "./src/vite_plugins/svgstore";
+import styleImport, { VantResolve } from "vite-plugin-style-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,16 @@ export default defineConfig({
       transformOn: true,
       mergeProps: true,
     }),
-    svgstore()
+    svgstore(),
+    styleImport({
+      resolves: [VantResolve()],
+      libs: [
+        {
+          libraryName: "vant",
+          esModule: true,
+          resolveStyle: (name) => `../es/${name}/style`,
+        },
+      ],
+    }),
   ],
 });

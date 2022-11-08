6518a6570ceafbc34960ee8dac7e6b42b4f56bff
lihao <lihao.coder@foxmail.com>
2022-11-08 23:51:43 +0800

    触发长按效果

--- src/components/item/Tags.tsx
+++ src/components/item/Tags.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, PropType } from "vue";
+import { defineComponent, PropType, ref } from "vue";
 import { RouterLink } from "vue-router";
 import { Button } from "../../shared/Button";
 import { http } from "../../shared/Http";
@@ -26,9 +26,36 @@ export const Tags = defineComponent({
       context.emit("update:selected", tag.id);
     };
 
+    const timer = ref<number>();
+    const currentTag = ref<HTMLDivElement>();
+    // let timer: number | undefined = undefined;
+    const onTouchstart = (e: TouchEvent) => {
+      currentTag.value = e.currentTarget as HTMLDivElement;
+      timer.value = setTimeout(() => {
+        onLongPress();
+      }, 1000);
+    };
+    const onTouchend = (e: TouchEvent) => {
+      clearTimeout(timer.value);
+    };
+    const onLongPress = () => {
+      console.log("长安");
+    };
+    const onTouchmove = (e: TouchEvent) => {
+      const pointedElement = document.elementFromPoint(
+        e.touches[0].clientX,
+        e.touches[0].clientY
+      );
+      if (
+        currentTag.value !== pointedElement &&
+        currentTag.value?.contains(pointedElement) === false
+      ) {
+        clearTimeout(timer.value);
+      }
+    };
     return () => (
       <>
-        <div class={s.tags_wrapper}>
+        <div class={s.tags_wrapper} onTouchmove={onTouchmove}>
           <RouterLink to={`/tags/create?kind=${props.kind}`} class={s.tag}>
             <div class={s.sign}>
               <Icon name="add" class={s.createTag} />
@@ -39,6 +66,8 @@ export const Tags = defineComponent({
             <div
               onClick={() => onSelect(tag)}
               class={[s.tag, props.selected === tag.id ? s.selected : ""]}
+              onTouchstart={onTouchstart}
+              onTouchend={onTouchend}
             >
               <div class={s.sign}>{tag.sign}</div>
               <div class={s.name}>{tag.name}</div>

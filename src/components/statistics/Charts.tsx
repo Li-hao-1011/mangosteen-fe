8e15ca4aa39e4035dfdce4d44c639c8f57e5a2a8
lihao <lihao.coder@foxmail.com>
2022-11-10 23:06:38 +0800

    请求lineChart数据，修复 ref chart 导致的问题

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,10 +1,14 @@
-import { defineComponent, PropType, ref } from "vue";
+import { computed, defineComponent, onMounted, PropType, ref } from "vue";
 import { FormItem } from "../../shared/Form";
 import s from "./Charts.module.scss";
 import * as echarts from "echarts";
 import { LineChart } from "./LineChart";
 import { PieChart } from "./PieChart";
 import { Bars } from "./Bars";
+import { http } from "../../shared/Http";
+
+type Data1Item = { happen_at: string; amount: number };
+type Data1 = Data1Item[];
 
 export const Charts = defineComponent({
   props: {
@@ -18,7 +22,32 @@ export const Charts = defineComponent({
     },
   },
   setup: (props, context) => {
-    const category = ref("支出");
+    const kind = ref("expenses");
+    const data1 = ref<Data1>([]);
+    const betterData1 = computed(() => {
+      return data1.value.map(
+        (item) => [item.happen_at, item.amount] as [string, number]
+      );
+    });
+
+    onMounted(async () => {
+      if (!props.startDate || !props.endDate) {
+        return;
+      }
+      const response = await http.get<{ groups: Data1; summary: number }>(
+        "/items/summary",
+        {
+          happen_after: props.startDate,
+          happen_before: props.endDate,
+          kind: kind.value,
+          _mock: "itemSummary",
+        }
+      );
+      console.log("response.data");
+      console.log(response.data);
+      data1.value = response.data.groups;
+    });
+
     return () => (
       <div class={s.wrapper}>
         <FormItem
@@ -28,10 +57,9 @@ export const Charts = defineComponent({
             { value: "expenses", text: "支出" },
             { value: "income", text: "收入" },
           ]}
-          v-model={category.value}
+          v-model={kind.value}
         />
-
-        <LineChart />
+        <LineChart data={betterData1.value} />
         <PieChart />
         <Bars />
       </div>

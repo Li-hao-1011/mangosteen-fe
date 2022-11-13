789fde7281beaa2a3e8c02e95ee742c8c9fd5fc6
lihao <lihao.coder@foxmail.com>
2022-11-13 22:12:34 +0800

    解决 onMounted 中出现error

--- src/hooks/useAfterMe.tsx
+++ src/hooks/useAfterMe.tsx
@@ -4,7 +4,25 @@ import { useMeStore } from '../stores/useMeStore'
 export const useAfterMe = (fn: () => void) => {
   const meStore = useMeStore()
   onMounted(async () => {
-    await meStore.mePromise
-    fn()
+    // omMounted 中不允许有未处理的error
+    // 写法一
+    /*    try {
+      await meStore.mePromise
+    } catch (error) {
+      return
+    } */
+    // 写法二
+    /*    const res = await meStore.mePromise?.catch((error) => new Error())
+    if (res instanceof Error) {
+      return
+    }
+    fn() */
+    // 写法三
+    meStore.mePromise?.then(
+      (res) => {
+        fn()
+      },
+      (error) => undefined
+    )
   })
 }

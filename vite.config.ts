26065acbf7f5b9024847c6aa835f0ba381b2227a
lihao <lihao.coder@foxmail.com>
2022-11-15 22:31:53 +0800

    优化: rollup chunk 打包

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
-import { svgstore } from "./src/vite_plugins/svgstore";
-import styleImport, { VantResolve } from "vite-plugin-style-import";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { svgstore } from './src/vite_plugins/svgstore'
+import styleImport, { VantResolve } from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,25 +10,45 @@ export default defineConfig({
     vue(),
     vueJsx({
       transformOn: true,
-      mergeProps: true,
+      mergeProps: true
     }),
     svgstore(),
     styleImport({
       resolves: [VantResolve()],
       libs: [
         {
-          libraryName: "vant",
+          libraryName: 'vant',
           esModule: true,
-          resolveStyle: (name) => `../es/${name}/style`,
-        },
-      ],
-    }),
+          resolveStyle: (name) => `../es/${name}/style`
+        }
+      ]
+    })
   ],
   server: {
     proxy: {
-      "/api/v1": {
-        target: "http://119.13.82.6:3000",
-      },
-    },
+      '/api/v1': {
+        target: 'http://119.13.82.6:3000'
+      }
+    }
   },
-});
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: any) {
+          if (id.includes('echarts')) {
+            return 'echarts'
+          }
+          if (id.includes('mock') || id.includes('faker')) {
+            return 'moke'
+          }
+          if (id.includes('vant')) {
+            return 'vant'
+          }
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        }
+      }
+    }
+  }
+})
